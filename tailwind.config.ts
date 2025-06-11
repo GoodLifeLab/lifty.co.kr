61c50e924a73a9dffafc800e2e8451beac26f96d
soyoung125 <sypark041754@gmail.com>
2025-06-11 18:01:32 +0900

    refactor: code formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   plugins: [],
-}
+};
