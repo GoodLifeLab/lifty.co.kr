2cad31ddf102e5e04de8c52644424bfa912757e0
soyoung125 <sypark041754@gmail.com>
2025-09-11 15:47:32 +0900

    refactor: add contact email and remove department

---
+++ prisma/migrations/20250911064711_add_contact_email_and_remove_department/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `department` on the `organizations` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "organizations" DROP COLUMN "department",
+ADD COLUMN     "contactEmail" TEXT;
